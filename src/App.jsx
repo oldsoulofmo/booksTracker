ed48ff059961956e024621a7822026640f954a57
Mohammed chaouki <99935485+oldsoulofmo@users.noreply.github.com>
2024-01-26 09:53:09 +0800

    Initial

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+import Header from './components/header'
+
+function App() {
+
+  return (
+  
+  <main>  
+    <Header />
+  </main>
+    
+  )
+}
+
+export default App
---
+++ src/components/header.jsx
@@ -0,0 +1,8 @@
+export default function(){
+    return <header>
+        <nav>
+            <li>booksTracker</li>
+            <li>Twitter</li>
+        </nav>
+    </header>
+}
