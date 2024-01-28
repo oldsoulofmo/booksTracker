c4f03cac3707eedcb95be041de2c69c182cc74e5
Mohammed chaouki <medchaouki12@icloud.com>
2024-01-29 05:19:34 +0800

    UI

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,28 @@ import "./App.css";
 
 export default function App() {
   const [isNight, setNight] = useState(false);
+  const [book, setBooks] = useState([]);
+
+  function addBooks(book) {
+    setBooks((books) => [...books, book]);
+  }
+
+  function handleNight() {
+    setNight(!isNight);
+  }
+
   return (
-    <div className={isNight ? "night" : ""}>
-      <Header
-        night={isNight}
-        onClick={() => setNight(true)}
-        onClick2={() => setNight(false)}
-      />
-      <div className="search-container">
-        <Search isNight={isNight} />
+    <main className={isNight ? "night" : ""}>
+      <div>
+        <Header night={isNight} onClick={handleNight} onClick2={handleNight} />
+        <div className="search-container">
+          <Search isNight={isNight} onAddBooks={addBooks} />
+        </div>
+        <div>
+          <BookList book={book} night={isNight} />
+        </div>
       </div>
-    </div>
+    </main>
   );
 }
 
@@ -33,12 +44,41 @@ function Header({ night, onClick, onClick2 }) {
   );
 }
 
-function Search({ isNight }) {
+function Search({ isNight, onAddBooks }) {
+  const [input, setInput] = useState("");
+  function handleForm(e) {
+    e.preventDefault();
+    if (!input) return;
+    const newBook = { name: input };
+    console.log(input);
+    console.log(newBook);
+    onAddBooks(newBook);
+    setInput("");
+  }
   return (
-    <section>
+    <form onSubmit={handleForm}>
       <input
+        type="text"
+        value={input}
         className={!isNight ? "textBox-day" : "textBox"}
-        placeholder="Enter your current reading book ..."></input>
-    </section>
+        placeholder="What are you reading ?"
+        onChange={(e) => setInput(e.target.value)}
+      />
+    </form>
   );
+}
+
+function BookList({ book }) {
+  return (
+    <ul>
+      {book.map((book) => (
+        <Book book={book} />
+      ))}
+    </ul>
+  );
+}
+
+function Book({ book }) {
+  console.log(book);
+  return <li>{book.name}</li>;
 }
